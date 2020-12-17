448801dbec7b6f09d3ac2dc579414eb58ab86b96
Bogdan OKner <bogdano@ip-10-155-222-26>
2020-12-17 18:01:01 +0200

    continuing building the script

--- arch_install.sh
+++ arch_install.sh
@@ -5,4 +5,6 @@ echo "which disk to install into?
 fdisk -l |grep /dev |awk '{print $2}' | sed 's/.$//' |grep -v -e loop -e sr
 read DISK 
 SIZE=`fdisk -l "$DISK" |grep GiB |awk '{print $3}'`
-echo "g\nn\np\n1\n\n+1G\nn\np\n\n\n+4G\nn\np\n\n\n+40G\nn\np\n\n\n\n"
+echo "g\nn\np\n1\n\n+1G\nn\np\n\n\n+4G\nn\np\n\n\n+40G\nn\np\n\n\n\n |fdisk $DISK"
+mkfs.fat -F32 "$DISK1" && mkswap "$DISK2" && mkfs.ext4 "DISK3" && mkfs.ext4 "DISK4"
+mount "$DISK3" /mnt && mkdir /mnt/boot && mkdir /mnt/home && mount "$DISK1" /mnt/boot && mount "$DISK4" /mnt/home && swapon
